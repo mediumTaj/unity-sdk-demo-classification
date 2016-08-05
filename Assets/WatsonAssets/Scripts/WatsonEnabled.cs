fe43203f1325b63253fd54a3f237378bd8233eaf
Ajiemar Santiago <ajiemar@gmail.com>
2016-08-04 20:12:25 -0500

    start watson enabled

---
+++ Assets/WatsonAssets/Scripts/WatsonEnabled.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using IBM.Watson.DeveloperCloud.Logging;
+using IBM.Watson.DeveloperCloud.Utilities;
+using IBM.Watson.DeveloperCloud.Debug;
+
+public class WatsonEnabled : MonoBehaviour
+{
+	void Start ()
+	{
+		LogSystem.InstallDefaultReactors();
+	}
+
+	//void OnEnable()
+	//{
+	//	EventManager.Instance.RegisterEventReceiver("OnShowDebug", OnShowDebug);
+	//}
+
+	//void OnDisable()
+	//{
+	//	EventManager.Instance.UnregisterEventReceiver("OnShowDebug", OnShowDebug);
+	//}
+
+	//private void OnShowDebug(object[] args)
+	//{
+	//	Log.Debug("WatsonEnabled", "Event received!");
+	//	//DebugConsole.Instance.
+	//}
+}
